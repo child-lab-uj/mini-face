f3ae78015108ad6d7baa693fc66fd4368b59e105
Jan Smółka <jsmolka@student.agh.edu.pl>
2024-09-09 19:36:48 +0200

    Added test script

--- dev/test.py
+++ dev/test.py
@@ -1,5 +1,15 @@
-import pathlib
 import sys
+import pathlib
 sys.path.append(str(pathlib.Path(__file__).parent.parent.absolute() / 'build'))
-print(*sys.path, sep=',\n')
+
 import GazeTracking
+import cv2
+
+if __name__ == "__main__":
+    image = cv2.imread("dev/data/test_image.jpg")
+    roi = (214.467, 96.9926, 110.877, 117.08)
+
+    extractor = GazeTracking.GazeExtractor()
+    extractor.estimate_camera_calibration(image)
+
+    print(extractor.detect_gaze(image, 0, roi))
